d5b86d0bb15f4dd3b5214e0927334fa913ec9049
Ossi <saurio.ossi@gmail.com>
2024-11-24 12:42:42 +0200

    Meistä sivu frontendiin.

--- varasto/src/components/Meista.jsx
+++ varasto/src/components/Meista.jsx
@@ -2,7 +2,29 @@ function Meista() {
     return (
       <div>
         <h1>Meistä</h1>
-        <p>Meistä-sivu</p>
+        <p>Tervetuloa Lemmikkitarvikeverkkokauppaan – paikkaan, jossa lemmikit ja heidän tarpeensa ovat sydämemme asia! 🐾 <br/>
+        <br/>
+        Vuonna 2024 kahden hännänheiluttajan, Omppu ja Rane -koirien intohimo parempaan lemmikinelämään synnytti tämän verkkokaupan. Omppu, joka tunnetaan uteliaana innovoijana, ja Rane, käytännöllisyyden mestari, päättivät yhdistää voimansa tarjotakseen korkealaatuisia ja huolella valikoituja tarvikkeita kaikille karvaisille, höyhenpeitteisille ja suomukkaisille ystävillemme. <br/>
+        <br/>
+        <img src="src\assets\kuvat\koirakuva.jpg" alt="Koira" width="684" height="296" /> <br/>
+        <br/>
+        Meidän tavoitteemme on tehdä lemmikkien elämästä onnellisempaa ja omistajien arjesta sujuvampaa. Jokainen tuotteemme on testattu ja hyväksytty Ompun ja Ranen tiukkojen standardien mukaisesti – nämä nelijalkaiset yrittäjät tietävät, mitä lemmikit oikeasti tarvitsevat ja rakastavat! <br/>
+        <br/>
+        Valikoimamme kattaa kaiken leluista, herkuista ja hoitotuotteista aina käytännöllisiin tarvikkeisiin, jotka sopivat jokaisen lemmikin ja omistajan elämäntyyliin. Lisäksi olemme sitoutuneet kestävyyteen ja ympäristöystävällisyyteen – haluamme huolehtia myös planeetastamme. <br/>
+        <br/>
+        <h3>Yhteystiedot:</h3>
+        Lemmikkitarvikeverkkokauppa <br/>
+        Karvakuonokatu 12 <br/>
+        00100 Helsinki <br/>
+        <br/>
+        Y-tunnus: 1234567-8 <br/>
+        <br/>
+        Jos sinulla on kysyttävää, palautetta tai tarvitset apua tilauksesi kanssa, älä epäröi ottaa meihin yhteyttä! <br/>
+        <br/>
+        Sähköposti: asiakaspalvelu@lemmikkitarvike.fi <br/>
+        Puhelin: 040-1234567 <br/>
+        <br/>
+        Kiitos, että valitsit Lemmikkitarvikeverkkokaupan – yhdessä teemme lemmikkien elämästä parempaa! 🐾</p>
       </div>
     );
   }
